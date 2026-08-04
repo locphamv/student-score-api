95723dd0a5d7bb8d0ac0bc474c1f4b793cc0bf4a
Loc Pham <249040321+locphamv@users.noreply.github.com>
2026-08-04 15:42:21 +0700

    Add endpoint to create students

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field
 
 
 # Create the FastAPI application
 app = FastAPI()
+
+
+class StudentCreate(BaseModel):
+    name: str = Field(min_length=1, max_length=50)
+    math: float = Field(ge=0, le=10)
+    english: float = Field(ge=0, le=10)
+    science: float = Field(ge=0, le=10)
+
 
 students = [
     {
@@ -52,3 +61,41 @@ def get_student(student_id: int):
         status_code=404,
         detail="Student not found",
     )
+
+
+@app.post(
+    "/students",
+    status_code=status.HTTP_201_CREATED,
+)
+def create_student(student: StudentCreate):
+    if students:
+        new_id = max(
+            student_item["id"]
+            for student_item in students
+        ) + 1
+    else:
+        new_id = 1
+
+    print(new_id)
+
+    average = round(
+        (
+            student.math
+            + student.english
+            + student.science
+        )
+        / 3,
+        2,
+    )
+
+    new_student = {
+        "id": new_id,
+        "name": student.name,
+        "math": student.math,
+        "english": student.english,
+        "science": student.science,
+        "average": average,
+    }
+
+    students.append(new_student)
+    return new_student
